f5f2532f1dc700eba3a7affe51ff3c6e9dbe111e
kie4280 <kie4280@livemail.tw>
2021-12-25 22:55:48 +0800

    acc 78.166

--- HW5/model.py
+++ HW5/model.py
@@ -9,6 +9,7 @@ import numpy as np
 
 save_path = "/kaggle/working/"
 input_path = "/kaggle/input/ml-hw5/"
+model_save_ = "/kaggle/input/notebook-ml-hw5/"
 # save_path = ""
 # input_path = ""
 
@@ -26,35 +27,39 @@ y_train: np.ndarray = oh.fit_transform(
     np.array(training_set["cuisine"]).reshape(-1, 1))
 
 X_test = mlb.transform(validation_set["ingredients"])
-print()
+print(X_train.shape)
 
 
 class MLP:
     def __init__(self, input_shape, load_model=False) -> None:
         data_in = keras.Input(shape=(input_shape, ))
-        x = keras.layers.Dense(
-            1024, activation='relu')(data_in)
-        x = keras.layers.Dense(1024, activation='relu')(x)
+        x = data_in
+        x = keras.layers.Dense(200, activation='relu')(x)
+        x = keras.layers.Dropout(0.2)(x)
+
+        x = keras.layers.Dense(200, activation='relu')(x)
+        x = keras.layers.Dropout(0.2)(x)
+        x = keras.layers.Dense(100, activation='relu')(x)
         x = keras.layers.Dense(20, activation='softmax')(x)
-        if os.path.exists(save_path + "my_model") and load_model:
-                        
+        if os.path.exists(model_save_ + "my_model") and load_model:
+
             print("model exists!")
-            self.model = keras.models.load_model(save_path + "my_model")
+            self.model = keras.models.load_model(model_save_ + "my_model")
         else:
             self.model = keras.Model(inputs=data_in, outputs=x)
 
     def train(self, X, y):
-        
-        self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.01),
-                           loss=keras.losses.MeanSquaredError(),
-                           metrics=[keras.metrics.Accuracy(), keras.metrics.Precision()])
+
+        self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
+                           loss=keras.losses.CategoricalCrossentropy(),
+                           metrics=[keras.metrics.CategoricalAccuracy(), keras.metrics.Precision()])
 
         history = self.model.fit(
-            X, y, epochs=60, validation_split=0.3)
+            X, y, batch_size=32,  epochs=3, validation_split=0.1)
         pass
 
     def forward(self, X):
-        
+
         y = self.model.predict(X)
         return y
 
@@ -63,13 +68,12 @@ class MLP:
         print(res)
 
 
-m = MLP(X_train.shape[1], load_model=True)
+m = MLP(X_train.shape[1], load_model=False)
 # print(m.model.summary())
 m.train(X_train, y_train)
 m.model.save(save_path + "my_model")
 y_pred = m.forward(X_test)
-y_pred = oh.inverse_transform(y_pred)
-print(y_pred)
+y_pred = oh.inverse_transform(y_pred, threshold=0.5)
 ss = pd.Series(y_pred.squeeze())
 outp = pd.DataFrame(columns=["id", "Category"])
 outp["id"] = validation_set["id"]
@@ -78,5 +82,3 @@ outp["Category"] = ss
 
 with open(save_path + "test_result.csv", 'w+') as f:
     f.write(outp.to_csv(index=None))
-
-
