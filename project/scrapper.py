2784116dc70e161781407334c5c9af5108def47d
kie4280 <kie4280@livemail.tw>
2022-01-10 14:29:51 -0600

    add initial scraped results

--- project/scrapper.py
+++ project/scrapper.py
@@ -8,26 +8,29 @@ headers = {"Ocp-Apim-Subscription-Key": subscription_key}
 
 search_term = ""
 sites_bait = ["buzzfeednews.com", "upworthy.com", "viralnova.com",
-              " BoredPanda.com", "Thatscoop.com", "thepoliticalinsider.com", "Examiner.net"]
+              "BoredPanda.com", "Thatscoop.com", "thepoliticalinsider.com", "Examiner.net"]
 
 sites_n_bait = ["nytimes.com", "washingtonpost.com",
                 "theguardian.com", "Bloomberg.com", "Reuters.com"]
 
 
 def search(keyword: str, site: str, size: int = 1000):
-
+    offset = 0
     params = {"q": "{} site:{}".format(
-        keyword, site), "textDecorations": False, "textFormat": "HTML", "count": 100}
-    response = requests.get(search_url, headers=headers, params=params)
-    response.raise_for_status()
-    search_results = response.json()
-    titles = [article["name"] for article in search_results["value"]]
+        keyword, site), "textDecorations": False, "textFormat": "HTML", "count": min(size - offset, 100), "offset": offset}
+    titles = []
+    while offset < size:
+        response = requests.get(search_url, headers=headers, params=params)
+        response.raise_for_status()
+        search_results = response.json()
+        titles += [article for article in search_results["value"]]
+        offset += 100
     return titles
 
 
-i = sites[1]
+i = sites_bait[0]
 
-ts = search("", i)
+ts = search("", i, 100)
 with open("{}.json".format(i), "w+") as f:
     print(ts)
     s = json.dumps(ts)
