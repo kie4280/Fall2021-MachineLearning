16c9fefe0f6e6c334c1015331026d9263e78b1d1
kie4280 <kie4280@livemail.tw>
2022-01-10 22:39:25 -0600

    1. combined bait and not bait dataset

--- project/scrapper.py
+++ project/scrapper.py
@@ -1,6 +1,10 @@
 import re
 import requests
 import json
+import time
+import glob
+import unidecode
+import html
 
 subscription_key = "fca1ffbacd424663a47f230d6890c7c3"
 search_url = "https://api.bing.microsoft.com/v7.0/news/search"
@@ -25,13 +29,49 @@ def search(keyword: str, site: str, size: int = 1000):
         search_results = response.json()
         titles += [article for article in search_results["value"]]
         offset += 100
+        time.sleep(1)
     return titles
 
 
-i = sites_bait[0]
+# for i in sites_bait:
+#     ts = search("", i, 1000)
+#     with open("bait/{}.json".format(i), "w+") as f:
+#         print(ts)
+#         s = json.dumps(ts)
+#         f.write(s)
 
-ts = search("", i, 100)
-with open("{}.json".format(i), "w+") as f:
-    print(ts)
-    s = json.dumps(ts)
-    f.write(s)
+
+# for i in sites_n_bait:
+#     ts = search("", i, 1000)
+#     with open("not_bait/{}.json".format(i), "w+") as f:
+#         print(ts)
+#         s = json.dumps(ts)
+#         f.write(s)
+
+bait = glob.glob("bait/*.json")
+n_bait = glob.glob("not_bait/*.json")
+
+bait_list = []
+for f in bait:
+    with open(f) as fo:
+        j = json.loads(fo.read())
+        l = [[unidecode.unidecode(html.unescape(x["name"])), 1] for x in j]
+        bait_list += l
+        del j
+
+
+n_bait_list = []
+for f in n_bait:
+    with open(f) as fo:
+        j = json.loads(fo.read())
+        l = [[unidecode.unidecode(html.unescape(x["name"])), 0] for x in j]
+        n_bait_list += l
+        del j
+
+with open("bait.json", "w") as f:
+    l = json.dumps(bait_list)
+    f.write(l)
+
+with open("not_bait.json", "w") as f:
+    l = json.dumps(n_bait_list)
+    f.write(l)
